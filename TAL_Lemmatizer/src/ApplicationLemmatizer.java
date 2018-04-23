fb06c4bc4e35e1ca817c88eecc1459c7d8a2354c
Guillaume H <guillaume.hurot@etu.umontpellier.fr>
2018-04-23 18:57:47 +0200

    Ajout de la recherche des classes grammaticales pour valider les mots

--- TAL_Lemmatizer/src/ApplicationLemmatizer.java
+++ TAL_Lemmatizer/src/ApplicationLemmatizer.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,29 +9,59 @@ import java.util.Map.Entry;
 
 import RequeterRezo.Mot;
 import RequeterRezo.RequeterRezo;
+import RequeterRezo.Terme;
 
 public class ApplicationLemmatizer {
 	
-	/* TODO : vérifier grace à l'api jdm que les mots générés existent */
-
 	public static void main(String[] args) {
+		RequeterRezo rezo = new RequeterRezo("72h", 5000);
+		HashMap<String, ArrayList<String>> res = new HashMap<String, ArrayList<String>>();
+		res.put("existe", new ArrayList<String>());
+		res.put("existePas", new ArrayList<String>());
+		res.put("erreurs", new ArrayList<String>());
+
+		
 		// Les regles sont regroupées par la terminaison du prédicat.
 		// Il suffit donc de parcourir les clés de la Map pour avoir toutes les terminaisons qu'on peut transformer.
 		Map<String, ArrayList<Rule>> categorie = new HashMap<String, ArrayList<Rule>>();
-		String nom = "regles";
+		String rulesFileName = "regles";
+		generateRule(categorie, rulesFileName);
+		System.out.println("- - - - - - - - - - Création des regles");
+		System.out.println(categorie);
 		
-		generateRule(categorie, nom);
-//		System.out.println("\n- - - - - - - - - - Création des regles");
-//		System.out.println(categorie);
 		
-		String mot = "manger";
-		ArrayList<String> lstDerivation = editWord(categorie, mot);
-		System.out.println("\n- - - - - - - - - - Création des mots dérivés de '" + mot + "'");
-		System.out.println(lstDerivation);
+		System.out.println("\n- - - - - - - - - - Mise en place du mot");
+		ArrayList<String> mot_gramm = new ArrayList<String>();
+		mot_gramm.add("manger");
+		mot_gramm.addAll(getClaGrammFromWord(rezo, getMot(rezo, mot_gramm.get(0), res.get("erreurs")), res.get("erreurs")));
+		System.out.println("Mot : '" + mot_gramm.get(0) + "'");
+		System.out.println("Classe grammaticale : '" + mot_gramm + "'");
 		
-		checkWords(lstDerivation);
-//		System.out.println("\n- - - - - - - - - - Vérification de l'existance des mots");
+		
+		System.out.println("\n- - - - - - - - - - Création des mots dérivés de '" + mot_gramm.get(0) + "'");
+		editWord(rezo, categorie, res, mot_gramm);
+		
+		
+		System.out.println("\n- - - - - - - - - - Résultats");
+		System.out.println("Les mots existent :");
+		System.out.println(res.get("existe"));
+		System.out.println("\nLes mots n'existent pas :");
+		System.out.println(res.get("existePas"));
+		System.out.println("\nLes mots non vérifiés (suite à une erreur) :");
+		System.out.println(res.get("erreurs"));
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	private static void generateRule(Map<String, ArrayList<Rule>> c, String nom) {
 		File f = new File(nom);
@@ -62,39 +91,91 @@ public class ApplicationLemmatizer {
 		}
 	}
 	
-	private static ArrayList<String> editWord(Map<String, ArrayList<Rule>> c, String mot) {
-		ArrayList<String> lstNewWords = new ArrayList<String>();
+	private static void editWord(RequeterRezo rezo, Map<String, ArrayList<Rule>> c, Map<String, ArrayList<String>> res, ArrayList<String> m) {
+		String mot = m.get(0);
+		ArrayList<String> existe = res.get("existe");
+		ArrayList<String> existePas = res.get("existePas");
+		ArrayList<String> err = res.get("erreurs");
 		
 		for (Entry<String, ArrayList<Rule>> entry : c.entrySet()) {
 			if (mot.endsWith(entry.getKey())) {
 				String base = mot.substring(0, mot.length()-entry.getKey().length());
-				for (Rule r : entry.getValue()) {
-					boolean finExc = false;
-					for (String s : r.getLstException()) {
-						if (base.endsWith(s)) finExc = true;
-					}
-					if (!finExc) {
-						String newWord = base + r.getNewSuffixe();
-						lstNewWords.add(newWord);
+				for (Rule r : entry.getValue()) { /* Parcours de toutes les regles qui match avec la terminaison */
+					for (String class_gramm : m) {
+					
+						if (r.getTag().equals(class_gramm)) { /* Verification que la regle s'applique bien pour ce mot (de cette classe grammaticale) */
+							/* Verification que le mot n'est pas dans les exceptions */
+							boolean finExc = false; // le mot n'y est pas
+							for (String s : r.getLstException()) { if (base.endsWith(s)) finExc = true; } // true si le mots est dans les exceptions
+							
+							if (!finExc) { // si pas exception
+								String newWord = base + r.getNewSuffixe(); // creation du nouveau mot
+								/* Verification que le nouveau mot existe bien */
+								Mot newMot = getMot(rezo, newWord, err);
+								if (newMot != null) {
+									/* Verification que le nouveau mot a bien la bonne class grammaticale */
+									ArrayList<String> classGram = getClaGrammFromWord(rezo, newMot, err);
+								
+									for (String class_gramm_new : classGram) {
+										// si la classe de la regle est la même que celle de rezo ou si la classe de rezo est null (non renseigné par exemple) ET pas d'erreur
+										if (r.getNewTag().equals(class_gramm_new) && !err.contains(newWord)) {
+											// Le mot existe, enlever du existe pas
+											if (!existe.contains(class_gramm_new + ":" + newWord))
+												existe.add(class_gramm_new + ":" + newWord);
+											if (existePas.contains(r.getNewTag() + ":" + newWord))
+												existePas.remove(class_gramm_new + ":" + newWord);
+										} else if (!err.contains(newWord)) {
+											if (!existePas.contains(r.getNewTag() + ":" + newWord) &&
+													!existe.contains(class_gramm_new + ":" + newWord))
+												existePas.add(r.getNewTag() + ":" + newWord);
+										}
+									}
+								} else if (!err.contains(newWord)) {
+									if (!existePas.contains(r.getNewTag() + ":" + newWord))
+										existePas.add(r.getNewTag() + ":" + newWord);
+								}
+							}
+						}
 					}
 				}
 			}
 		}
-		return lstNewWords;
 	}
-
-	private static void checkWords(ArrayList<String> lstDeriv) {
+	
+	public static Mot getMot(RequeterRezo rezo, String s, ArrayList<String> err) {
 		try {
-			RequeterRezo systeme = new RequeterRezo("48h", 3000);
-			Mot mot = systeme.requete("chat");
-//			System.out.println(mot.getDefinition());
-			// check pour chaque mot s'il existe
-		} catch(InterruptedException e) {
-			e.printStackTrace();
-		} catch(MalformedURLException e) {
-			e.printStackTrace();
-		} catch(IOException e) {
-			e.printStackTrace();
+			Mot m = rezo.requete(s, true);
+			return m;
+		} catch (IOException | InterruptedException | ArrayIndexOutOfBoundsException e) {
+			err.add(s);
 		}
+		return null;
+	}
+	
+	
+	public static ArrayList<String> getClaGrammFromWord(RequeterRezo r, Mot m, ArrayList<String> err) {
+		HashMap<String, ArrayList<Terme>> req = m.getRelations_sortantes();
+		ArrayList<Terme> termes = req.get("r_pos");
+		ArrayList<String> sortie = new ArrayList<String>();
+		for (Terme t : termes) {
+			String classe = null;
+			switch (t.getTerme()) {
+			case "Adj:" :
+				classe = "adj";
+				break;
+			case "Nom:" :
+				classe = "nom";
+				break;
+			case "Ver:Inf" :
+			case "Ver:PPre" :
+			case "Ver:" :
+				classe = "verbe";
+				break;
+//			default :
+//				sortie.add(t.getTerme());
+			}
+			if (!sortie.contains(classe) && classe != null) sortie.add(classe);
+		}
+		return sortie;
 	}
 }
